14e549ffe10228c1f2c536d001d458f9e9d5cd1a
hironomiu <hironomiu@gmail.com>
2022-04-19 16:59:46 +0900

    commited

--- src/api/v1/boards.ts
+++ src/api/v1/boards.ts
@@ -5,6 +5,7 @@ const boards = Router()
 const prisma = new PrismaClient()
 
 boards.route('/').get(async (req, res) => {
+  // TODO: 一旦user_idは固定（将来は認証後sessionでuser_idを持つ)
   const boards = await prisma.boards.findMany({
     where: {
       user_id: 1,
--- src/api/v1/todos.ts
+++ src/api/v1/todos.ts
@@ -5,6 +5,7 @@ const todos = Router()
 const prisma = new PrismaClient()
 
 todos.route('/').get(async (req, res) => {
+  // TODO: 一旦user_idは固定（将来は認証後sessionでuser_idを持つ)
   const todos = await prisma.todos.findMany({
     where: {
       user_id: 1,
